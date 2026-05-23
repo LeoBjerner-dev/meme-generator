d9be99397a6fdf5765681ebee63275bbd6a3e441
LeoBjerner-dev <leonard.bjerner@gmail.com>
2026-05-23 15:53:52 +0200

    added inputs without logic

--- src/components/MemeDisplay/MemeDisplay.tsx
+++ src/components/MemeDisplay/MemeDisplay.tsx
@@ -1,16 +1,25 @@
-import styles from "./MemeDisplay.module.css"
-import type { Imemes } from '../../../interfaces';
-
+import styles from "./MemeDisplay.module.css";
+import type { Imemes } from "../../../interfaces";
 
 const MemeDisplay = (props: Imemes) => {
-
   return (
     <div className={styles.MemeContainer}>
-        <div className={styles.ImageContainer}>
-            <img src={props.img} alt={props.alt} />
+      <div className={styles.ImageContainer}>
+        <img src={props.img} alt={props.alt} />
+        <br />
+        <div className={styles.textControls}>
+          <div className={styles.textBox}>
+            <input type="range" />
+            <input type="text" placeholder="Memetext 1"/>
+          </div>
+          <div className={styles.textBox}>
+            <input type="range" />
+            <input type="text" placeholder="Memetext 2"/>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MemeDisplay
+export default MemeDisplay;
